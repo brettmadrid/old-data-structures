438a5b8f82381514e37386ec7c67634e10830f7c
brettmadrid <brettmadrid9@gmail.com>
2019-09-12 20:12:21 -0700

    finished max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,20 +7,32 @@ class Heap:
     self.storage.append(value)
     value_index = len(self.storage) - 1
     self._bubble_up(value_index)
+    print(value)
 
   def delete(self):
-    # store what's at the front
-    # put the smallest value at the front, then remove it from our storage
-    # call sift down
-    # return value
+    if not self.storage:
+      return False
+    if self.get_size() == 1:
+      return self.storage.pop()
+    self.storage[0], self.storage[-1] = self.storage[-1], self.storage[0]
+    temp = self.storage.pop()
+    self._sift_down(0)
+    return temp
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
+    p = (index -1) // 2
+
+    if index <= 0:
+      return
+    elif self.storage[index] > self.storage[p]:
+      self.storage[index], self.storage[p] = self.storage[p], self.storage[index]
+      self._bubble_up(p)
     ## while index greater than 0
     ## get the parent (index - 1) // 2
     ## if child is greater than parent--> comparator(child, parent)
@@ -29,9 +41,17 @@ class Heap:
     ## break
 
   def _sift_down(self, index):
-    ## while index is less than max index
-    ## look at both children, choose the biggest
     ## left child: 2 * index, + 1
-    ## right child: 2 * index, + 2
-    ## swap with that child, if the chosen one is larger, update the index to the new location
-    ## otherwise break out out of your loop
+    left = (2 * index) + 1
+    # ## right child: 2 * index, + 2
+    right = (2 * index) + 2
+    da = index
+
+    if self.get_size() > left and self.storage[da] < self.storage[left]:
+      da = left
+    if self.get_size() > right and self.storage[da] < self.storage[right]:
+      da = right
+    if da != index:
+      self.storage[index], self.storage[da] = self.storage[da], self.storage[index]
+      self._sift_down(da)
+
